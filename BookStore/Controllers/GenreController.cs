52df1aeb4fd6a0aa30ef6c29fb882404fd9de524
roysa <roysa@KALI>
2025-08-16 13:19:17 +0530

    Add project files.

---
+++ BookStore/Controllers/GenreController.cs
@@ -0,0 +1,76 @@
+﻿using BookStore.Models.Domain;
+using BookStore.Repositories.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class GenreController : Controller
+    {
+        private readonly IGenreService service;
+        public GenreController(IGenreService service)
+        {
+            this.service = service;
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Genre model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var result = service.Add(model);
+            if(result)
+            {
+                TempData["msg"] = "Added successfully";
+                return RedirectToAction(nameof(Add));
+            }
+            TempData["msg"] = "Error has occured on server side";
+            return View(model);
+        }
+
+        public IActionResult Update(int id)
+        {
+            var record = service.FindById(id);
+            return View(record);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Genre model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var result = service.Update(model);
+            if (result)
+            {
+                TempData["msg"] = "Updated successfully";
+                return RedirectToAction("GetAll");
+            }
+            TempData["msg"] = "Error has occured on server side";
+            return View(model);
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            var result = service.Delete(id);
+            TempData["msg"] = "Deleted successfully";
+            return RedirectToAction("GetAll");
+
+        }
+
+        public IActionResult GetAll()
+        {
+            var data = service.GetAll();
+            return View(data);
+
+        }
+
+    }
+}
